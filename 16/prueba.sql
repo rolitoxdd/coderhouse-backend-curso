7e5803f8f90980343543273c54e5567bdf1ffe3b
rolitoxdd <rolo.alv97@gmail.com>
2021-10-01 11:48:00 -0300

    desafio 16

--- 16/prueba.sql
+++ 16/prueba.sql
@@ -1,5 +1,7 @@
+-- CREAR BDD LLAMADA 'PRUEBA'
 CREATE DATABASE prueba;
 
+-- CREAR TABLA 'ITEMS'
 CREATE TABLE prueba.items(
     nombre VARCHAR(20) NOT NULL,
     categoria VARCHAR(20) NOT NULL,
@@ -7,7 +9,20 @@ CREATE TABLE prueba.items(
     id INT PRIMARY KEY AUTO_INCREMENT
 );
 
+-- INSERTAR 3 REGISTROS
 INSERT INTO prueba.items(nombre, categoria, stock)
 VALUES ('fideos', 'harina', 20),
     ('leche', 'lacteos', 30),
     ('crema', 'lacteos', 15);
+
+-- LISTAR REGISTROS AGREGADOS
+SELECT * FROM prueba.items WHERE ID=1 OR ID=2 OR ID=3;
+
+-- BORRAR ITEM CON ID=1
+DELETE FROM prueba.items WHERE id=1;
+
+-- ACTUALIZAR STOCK DEL ITEM ID=2 A 45
+UPDATE prueba.items SET stock=45 WHERE id=2;
+
+-- LISTAR LOS REGISTROS 
+SELECT * FROM prueba.items;
